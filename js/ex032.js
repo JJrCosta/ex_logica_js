33d132375e13d4e0221eeae8b661efe6d7ba617c
JJrCosta <jrpercosta@gmail.com>
2022-03-07 21:16:34 -0300

    Update ex032.js
    Incrementando a condição caso o jogador escolha um numero fora do intervalo entre 1 e 5.

--- js/ex032.js
+++ js/ex032.js
@@ -2,10 +2,14 @@ function jogar () {
     const msg = document.querySelector('div[class="resultado"]');
     const player = document.querySelector('input[name="pitaco"]').value;
     const pc = Math.round((Math.random() * (4 - 0 + 0) + 1));
-    if (player == pc) {
-        msg.innerHTML = "ACERTOU!";
-    } else {
-        msg.innerHTML = "ERROU!";
+    if (player < 6 && player > 0) {
+        if (player == pc) {
+            msg.innerHTML = "ACERTOU!";
+        } else {
+            msg.innerHTML = "ERROU!";
+        }
+        msg.innerHTML += `Eu escolhi ${pc}.`;  
+    }else{
+        msg.innerHTML = `O número ${player} não está entre 1 e 5, escolha novamente.`;
     }
-    msg.innerHTML += `Eu escolhi ${pc}.`;
 }
